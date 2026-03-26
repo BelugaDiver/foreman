246ee22b070377711468eae67cde3c67f43c6d63
Tobi Adegunloye <tobiadegunloye@Tobis-MacBook-Pro.local>
2026-03-25 19:38:50 -0500

    feat: add pgcrypto extension migration for gen_random_uuid support

--- migrations/versions/0006_create_styles_table.py
+++ migrations/versions/0006_create_styles_table.py
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "0006"
-down_revision: Union[str, None] = "0005"
+down_revision: Union[str, None] = "0000"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
